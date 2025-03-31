335a118634a26734dc1483f04449158373f75f24
markg <17348049+sockcrates@users.noreply.github.com>
2025-03-31 18:37:11 -0400

    Update theme

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,47 @@
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'rebelot/kanagawa.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('kanagawa').setup {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = false },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {
+          -- ['@function'] = { fg = colors.palette.roninYellow, italic = false },
+          -- ['@keyword'] = { fg = colors.palette.peachRed, italic = false },
+          -- ['@keyword.function'] = {
+          --   fg = colors.palette.peachRed,
+          --   italic = false,
+          -- },
+        }
+      end,
+      theme = 'wave', -- Load "wave" theme
+      background = { -- map the value of 'background' option to a theme
+        dark = 'wave', -- try "dragon" !
+        light = 'lotus',
+      },
+    }
+
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'kanagawa'
+  end,
+}
