28c876aa1fd593dfc6cbd95afd80275ff462cdbd
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2025-04-21 11:38:04 -0400

    Format line length

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -46,7 +46,10 @@ return {
     --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
     --    function will be executed to configure the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      group = vim.api.nvim_create_augroup(
+        'kickstart-lsp-attach',
+        { clear = true }
+      ),
       callback = function(event)
         -- NOTE: Remember that Lua is a real programming language, and as such it is possible
         -- to define small helper and utility functions so you don't have to repeat yourself.
@@ -55,33 +58,62 @@ return {
         -- for LSP related items. It sets the mode, buffer and description for us each time.
         local map = function(keys, func, desc, mode)
           mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          vim.keymap.set(
+            mode,
+            keys,
+            func,
+            { buffer = event.buf, desc = 'LSP: ' .. desc }
+          )
         end
 
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map(
+          'gd',
+          require('telescope.builtin').lsp_definitions,
+          '[G]oto [D]efinition'
+        )
 
         -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map(
+          'gr',
+          require('telescope.builtin').lsp_references,
+          '[G]oto [R]eferences'
+        )
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map(
+          'gI',
+          require('telescope.builtin').lsp_implementations,
+          '[G]oto [I]mplementation'
+        )
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map(
+          '<leader>D',
+          require('telescope.builtin').lsp_type_definitions,
+          'Type [D]efinition'
+        )
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        map(
+          '<leader>ds',
+          require('telescope.builtin').lsp_document_symbols,
+          '[D]ocument [S]ymbols'
+        )
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map(
+          '<leader>ws',
+          require('telescope.builtin').lsp_dynamic_workspace_symbols,
+          '[W]orkspace [S]ymbols'
+        )
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
@@ -89,7 +121,12 @@ return {
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+        map(
+          '<leader>ca',
+          vim.lsp.buf.code_action,
+          '[C]ode [A]ction',
+          { 'n', 'x' }
+        )
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
@@ -114,8 +151,18 @@ return {
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+        if
+          client
+          and client_supports_method(
+            client,
+            vim.lsp.protocol.Methods.textDocument_documentHighlight,
+            event.buf
+          )
+        then
+          local highlight_augroup = vim.api.nvim_create_augroup(
+            'kickstart-lsp-highlight',
+            { clear = false }
+          )
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
@@ -129,10 +176,16 @@ return {
           })
 
           vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            group = vim.api.nvim_create_augroup(
+              'kickstart-lsp-detach',
+              { clear = true }
+            ),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              vim.api.nvim_clear_autocmds {
+                group = 'kickstart-lsp-highlight',
+                buffer = event2.buf,
+              }
             end,
           })
         end
@@ -141,9 +194,18 @@ return {
         -- code, if the language server you are using supports them
         --
         -- This may be unwanted, since they displace some of your code
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        if
+          client
+          and client_supports_method(
+            client,
+            vim.lsp.protocol.Methods.textDocument_inlayHint,
+            event.buf
+          )
+        then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            vim.lsp.inlay_hint.enable(
+              not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }
+            )
           end, '[T]oggle Inlay [H]ints')
         end
       end,
@@ -183,7 +245,11 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    capabilities = vim.tbl_deep_extend(
+      'force',
+      capabilities,
+      require('cmp_nvim_lsp').default_capabilities()
+    )
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -254,7 +320,12 @@ return {
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          server.capabilities = vim.tbl_deep_extend(
+            'force',
+            {},
+            capabilities,
+            server.capabilities or {}
+          )
           require('lspconfig')[server_name].setup(server)
         end,
       },
