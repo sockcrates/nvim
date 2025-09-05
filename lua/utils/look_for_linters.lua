34d0d250e89a45568b65ed4a5af01592b5e0639d
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2025-09-04 21:52:30 -0400

    Add Biome finder

--- lua/utils/look_for_linters.lua
+++ lua/utils/look_for_linters.lua
@@ -72,4 +72,39 @@ function M.find_eslint()
   return false
 end
 
+local biome_config_files = {
+  'biome.json',
+  'biome.jsonc',
+}
+
+local has_biome_cached = nil
+
+--- Searches for biome configuration files in the current directory or nearest
+--- git root.
+---
+--- @return boolean True if any biome config file is found, false otherwise.
+function M.find_biome()
+  if has_biome_cached ~= nil then
+    return has_biome_cached
+  end
+
+  local files = require 'utils.files'
+  if files.dir_contains_set_of_files(vim.fn.getcwd(), biome_config_files) then
+    has_biome_cached = true
+    return true
+  end
+
+  local git = require 'utils.git'
+  local git_root = git.find_nearest_git_root()
+  if git_root then
+    if files.dir_contains_set_of_files(git_root, biome_config_files) then
+      has_biome_cached = true
+      return true
+    end
+  end
+
+  has_biome_cached = true
+  return false
+end
+
 return M
