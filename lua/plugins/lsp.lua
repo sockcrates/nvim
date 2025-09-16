f9cca9efedd4af7af5a71bdd0e272cf51d23989c
sockcrates <17348049+sockcrates@users.noreply.github.com>
2025-09-16 14:32:25 -0400

    Add texlab

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -284,6 +284,7 @@ return {
       },
       pyright = {},
       rust_analyzer = {},
+      texlab = {},
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
